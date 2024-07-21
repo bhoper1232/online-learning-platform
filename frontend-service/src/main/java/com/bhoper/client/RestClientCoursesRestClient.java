d16bad0e7e96b1eca4fc3d4631be4b089078977b
Andrey Solianyk <solianik123456@gmail.com>
2024-07-21 15:58:49 +0200

    Added Keycloak

--- frontend-service/src/main/java/com/bhoper/client/RestClientCoursesRestClient.java
+++ frontend-service/src/main/java/com/bhoper/client/RestClientCoursesRestClient.java
@@ -1,11 +1,21 @@
 package com.bhoper.client;
 
 import com.bhoper.model.Course;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestClient;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 public class RestClientCoursesRestClient implements CoursesRestClient {
 
     private static final ParameterizedTypeReference<List<Course>> PRODUCTS_TYPE_REFERENCE =
@@ -14,8 +24,27 @@ public class RestClientCoursesRestClient implements CoursesRestClient {
 
     private RestClient restClient;
 
-    public RestClientCoursesRestClient(RestClient restClient) {
-        this.restClient = RestClient.builder().build();
+    private final OAuth2AuthorizedClientManager authorizedClientManager;
+
+    public RestClientCoursesRestClient(RestClient restClient,
+                                       ClientRegistrationRepository clientRegistrationRepository,
+                                       OAuth2AuthorizedClientRepository authorizedClientRepository) {
+        this.authorizedClientManager = new DefaultOAuth2AuthorizedClientManager(
+                clientRegistrationRepository, authorizedClientRepository);
+        this.restClient = RestClient.builder()
+                .requestInterceptor((request, body, execution) -> {
+                    if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                        String token = this.authorizedClientManager.authorize(OAuth2AuthorizeRequest
+                                .withClientRegistrationId("greetings-app-authorization-code")
+                                .principal(SecurityContextHolder.getContext().getAuthentication())
+                                .build()).getAccessToken().getTokenValue();
+
+                        request.getHeaders().setBearerAuth(token);
+                    }
+
+                    return execution.execute(request, body);
+                })
+                .build();
     }
 
     @Override
--- frontend-service/src/main/java/com/bhoper/config/ClientBeans.java
+++ frontend-service/src/main/java/com/bhoper/config/ClientBeans.java
@@ -1,17 +1,24 @@
 package com.bhoper.config;
 
 import com.bhoper.client.RestClientCoursesRestClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestClient;
 
 @Configuration
+@RequiredArgsConstructor
 public class ClientBeans {
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
+    private final OAuth2AuthorizedClientRepository authorizedClientRepository;
 
 
     @Bean
     public RestClientCoursesRestClient productsRestClient() {
-        return new RestClientCoursesRestClient(RestClient.builder().build());
+        return new RestClientCoursesRestClient(RestClient.builder().build(), clientRegistrationRepository, authorizedClientRepository);
     }
 
 }
