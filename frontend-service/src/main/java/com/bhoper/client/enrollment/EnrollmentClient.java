ba65932a005156e19569b716da2b7b30ecb61bec
Andrey Solianyk <solianik123456@gmail.com>
2024-08-05 22:45:49 +0200

    Dockerize the application and add nginx as a reverse proxy server

--- frontend-service/src/main/java/com/bhoper/client/enrollment/EnrollmentClient.java
+++ frontend-service/src/main/java/com/bhoper/client/enrollment/EnrollmentClient.java
@@ -3,7 +3,6 @@ package com.bhoper.client.enrollment;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
